a79601cf5681748e7e8130d2d37eba88cce7990e
soogary <38732384+soogary@users.noreply.github.com>
2024-05-10 17:44:11 +0000

    add

--- 2.py
+++ 2.py
@@ -51,7 +51,7 @@ for chunk in llm.stream(prompt):
     print(chunk.content, end='', flush=True) 
 
 
-#Prompt Templates / Chat Prompt Templates
+#Prompt Templates / 
 from langchain.prompt import PromptTemplate
 from langchain_openai import ChatOpenAI
 template = '''your are an expenienced virologist. write a few sentences about the following {virus} in {language}'''
@@ -63,3 +63,85 @@ prompt
 llm = ChatOpenAI(model_name='gpt-3.5.turbo', tempaerature=0)
 output = llm.invoke(prompt)
 print(output.content)
+
+
+#Chat Prompt Templates
+#create a output following system output in json format
+
+from langchain.prompts import ChatPromptTemplate, HumanMessagePromptTemplate
+from langchain_core.messages import SystemMessage
+
+chat_template = ChatPromptTemplate.from_message(
+
+    SystemMessage(content='You reposnd only in the JSON format'),
+    HumanMessagePromptTemplate.from_template('Top {n} countries in {area} by population')
+
+)
+
+messages = chat_template.format_messages(n='10', area='Europe')
+print(messages)
+
+from langchain_openai import ChatOpenAI
+llm = ChatOpenAI()
+output = llm.invoke(messages)
+print(output.content) 
+
+
+#Simple chains using prompt template with 2 variables as dictionary
+from langchain_openai import ChatOpenAI
+from langchain import PromptTemplate
+from langchain.chains import LLMChain
+
+llm - ChatOpenAI()
+template = '''your are an expenienced virologist. write a few sentences about the following {virus} in {language}'''
+prompt_template = PromptTemplate.from_template(template = template)
+
+chain = LLMChain(
+    llm=llm,
+    prompt=prompt_template
+#    verbose=True
+)
+
+output = chain.invoke({'virus': 'HSV', 'language': 'Spanish'})
+print(output)
+
+#Simple chains using template with 1 variable as string
+template = 'What is the capital of {country}?. List the top places to visit in that city. Use bullet points'
+prompt_template = PromptTemplate.from_template(template = template)
+
+chain = LLMChain(
+    llm=llm,
+    prompt=prompt_template
+#    verbose=True
+)
+
+country = input('Enter Country: ')
+output = chain.invoke(country)
+print(output['text'])
+
+
+#Sequential Chains (simple)
+# chain 2 takes the output of chain 1 as its input
+
+from langchain_openai import ChatOpenAI
+from langchain import PromptTemplate
+from langchain.chains import LLMChain, SimpleSequentialChain
+
+llm1 = ChatOpenAI(model_name ='gpt-3.5-turbo', temparature=1.2)
+prompt_template1 = PromptTemplate.from_template(
+        template='You are an experienced scientiest and Python programmer. Write a function that implements the concept of {concept}.'
+)
+chain1 = LLMChain(llm=llm1, prompt=prompt_template1)
+
+
+llm2 = ChatOpenAI(model_name ='gpt-4.5-turbo-preview', temparature=0.5)
+prompt_template2 = PromptTemplate.from_template(
+        template='Given the Pythion function {function} describe it in as much detail as possible.'
+)
+chain2 = LLMChain(llm=llm2, prompt=prompt_template2)
+
+overall_chain = SimpleSequentialChain(chains=[chain1, chain2], verbose=True)
+output = overall_chain.invoke('linear reqgression')
+
+print(output['output'])
+
