5ad102843a0c9ab2013d47585f951d6389e4fb65
soogary <38732384+soogary@users.noreply.github.com>
2024-05-10 08:35:17 -0700

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,6 +1,19 @@
 import numpy as np
+import pandas as pd
+import plotly.express as px
 
 #generate random numbers from uniform distribution between 0 and 1
 
 random_nums = np.random.rand(10)
 print(random_nums)
+
+
+#generate random numbers from a normal distribution
+data = pf.Dataframe({'x':np.arange(30), 'y': np.random.randn(30)})
+
+
+#create line chart
+fig = px.line(data, x='x', y = 'y')
+
+#show chart
+fig.show()
